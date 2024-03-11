3b71217c2fe7543986ab6b9fd58141b615f897be
Prayuj Tuli <prayujtuli@hotmail.com>
2024-03-11 17:07:13 -0400

    added nginx namespace, created nfs at new path

---
+++ nginx/update_configmaps.sh
@@ -0,0 +1,13 @@
+kubectl delete configmap nginx-config
+kubectl delete configmap nginx-certs
+kubectl delete configmap prayujt-website
+
+rm *.tar.gz
+tar -czvf nginx-config.tar.gz --directory=/etc/nginx .
+tar -czvf nginx-certs.tar.gz --directory=/etc/letsencrypt .
+tar -czvf prayujt-website.tar.gz --directory=/var/www/prayujt.com .
+
+kubectl create configmap nginx-config -n nginx --from-file=nginx-config.tar.gz
+kubectl create configmap nginx-certs -n nginx --from-file=nginx-certs.tar.gz
+kubectl create configmap prayujt-website -n nginx --from-file=prayujt-website.tar.gz
+
