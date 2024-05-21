d81c6210728eef16d0fc0ec6777f4abe32278a16
Prayuj Tuli <prayujtuli@hotmail.com>
2024-05-20 21:49:01 -0400

    fixed logout redirection

--- hydra/hydra-login/src/routes/logout/+page.svelte
+++ hydra/hydra-login/src/routes/logout/+page.svelte
@@ -1,26 +1,25 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { goto } from "$app/navigation";
+    import { onMount } from "svelte";
 
     let challenge = "";
 
     onMount(async () => {
         const urlParams = new URLSearchParams(window.location.search);
-        challenge = urlParams.get('logout_challenge') || "";
+        challenge = urlParams.get("logout_challenge") || "";
     });
 
     const handleLogout = async (accept) => {
-        const res = await fetch('/logout', {
-            method: 'POST',
+        const res = await fetch("/logout", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({ logout_challenge: challenge, accept }),
         });
 
         if (res.ok) {
             const { redirect_to } = await res.json();
-            goto(redirect_to);
+            window.location.href = redirect_to;
         } else {
             console.log("Logout failed");
         }
@@ -31,12 +30,16 @@
     <div class="m-auto w-96">
         <h1 class="mb-4">Logout</h1>
         <p class="mb-4">Do you really want to log out?</p>
-        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                on:click={() => handleLogout(true)}>
+        <button
+            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            on:click={() => handleLogout(true)}
+        >
             Yes
         </button>
-        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                on:click={() => handleLogout(false)}>
+        <button
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            on:click={() => handleLogout(false)}
+        >
             No
         </button>
     </div>
