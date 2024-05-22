5f0966a248754a9c90315a4475e8038dda02ebde
Prayuj Tuli <prayujtuli@hotmail.com>
2024-05-22 02:44:14 -0400

    moved file paths

--- nginx/update_nginx.sh
+++ nginx/update_nginx.sh
@@ -2,14 +2,14 @@ kubectl delete configmap nginx-config -n nginx
 kubectl delete configmap nginx-certs -n nginx
 kubectl delete configmap prayujt-website -n nginx
 
-rm *.tar.gz
-tar -czvf nginx-config.tar.gz --directory=/etc/nginx .
-tar -czvf nginx-certs.tar.gz --directory=/etc/letsencrypt .
-tar -czvf prayujt-website.tar.gz --directory=/var/www/prayujt.com .
+rm /root/kubeconfigs/nginx/*.tar.gz
+tar -czvf /root/kubeconfigs/nginx/nginx-config.tar.gz --directory=/etc/nginx .
+tar -czvf /root/kubeconfigs/nginx/nginx-certs.tar.gz --directory=/etc/letsencrypt .
+tar -czvf /root/kubeconfigs/nginx/prayujt-website.tar.gz --directory=/var/www/prayujt.com .
 
-kubectl create configmap nginx-config -n nginx --from-file=nginx-config.tar.gz
-kubectl create configmap nginx-certs -n nginx --from-file=nginx-certs.tar.gz
-kubectl create configmap prayujt-website -n nginx --from-file=prayujt-website.tar.gz
+kubectl create configmap nginx-config -n nginx --from-file=/root/kubeconfigs/nginx/nginx-config.tar.gz
+kubectl create configmap nginx-certs -n nginx --from-file=/root/kubeconfigs/nginx/nginx-certs.tar.gz
+kubectl create configmap prayujt-website -n nginx --from-file=/root/kubeconfigs/nginx/prayujt-website.tar.gz
 
 kubectl delete -f /root/kubeconfigs/nginx/nginx.yaml
 sleep 1
