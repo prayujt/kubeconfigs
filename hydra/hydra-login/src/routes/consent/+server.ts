006565615a02f370ea37ad5c3011624a9b0a322d
Prayuj Tuli <prayujtuli@hotmail.com>
2024-05-26 16:34:04 -0400

    fixed crashing bug in consent

--- hydra/hydra-login/src/routes/consent/+server.ts
+++ hydra/hydra-login/src/routes/consent/+server.ts
@@ -10,13 +10,14 @@ declare interface Session {
 declare interface Identity {
   email?: string;
   name?: string;
+  username?: string;
 }
 
 declare interface Account {
   id: string;
   email: string;
-  first_name: string;
-  last_name: string;
+  firstName: string;
+  lastName: string;
   username: string;
 }
 
@@ -27,14 +28,14 @@ const buildSession = (grant_scope: string[], user: Account): Session => {
     id_token: {
       ...(grant_scope.includes("email") && { email: user.email }),
       ...(grant_scope.includes("profile") && {
-        name: `${user.first_name} ${user.last_name}`,
+        name: `${user.firstName} ${user.lastName}`,
         username: user.username,
       }),
     },
     access_token: {
       ...(grant_scope.includes("email") && { email: user.email }),
       ...(grant_scope.includes("profile") && {
-        name: `${user.first_name} ${user.last_name}`,
+        name: `${user.firstName} ${user.lastName}`,
         username: user.username,
       }),
     },
@@ -74,18 +75,16 @@ export const POST: RequestHandler = async ({ request }) => {
       `${HYDRA_ADMIN_URL}/admin/oauth2/auth/requests/consent?consent_challenge=${consent_challenge}`,
     );
 
-    // const sql = postgres(
-    //   `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}`,
-    // );
-
-    // const user: Account[] =
-    //   (await sql`SELECT id,email,first_name,last_name,username FROM accounts WHERE email=${consentRequest.subject}`) as unknown as Account[];
-    // if (user.length === 0) {
-    //   return new Response(JSON.stringify({ message: "User not found" }), {
-    //     status: 404,
-    //     headers: { "Content-Type": "application/json" },
-    //   });
-    // }
+    console.log(identity);
+    if (!identity) {
+      return new Response(
+        JSON.stringify({ message: "No user information found" }),
+        {
+          status: 404,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    }
 
     if (!granted) {
       const { data: body } = await axios.put(
@@ -138,6 +137,8 @@ export const POST: RequestHandler = async ({ request }) => {
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
+    console.log(error);
+    console.log(error.message);
     return new Response(
       JSON.stringify({ message: "Error during consent handling" }),
       { status: 500, headers: { "Content-Type": "application/json" } },
